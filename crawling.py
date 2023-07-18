b8b342ace83cd7efeab27b207bc29f4fe7870903
Yongmin Kwon <rnjs5540@gmail.com>
2023-07-18 23:38:14 +0900

    [#1]category 추가, refactoring

--- crawling.py
+++ crawling.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105"
 headers = requests.utils.default_headers()
 headers.update(
     {
@@ -9,36 +8,48 @@ headers.update(
     }
 )
 
-response = requests.get(url, headers=headers)
+category_dict = {
+    '100': '정치',
+    '101': '경제',
+    '102': '사회',
+    '103': '생활/문화',
+    '104': '세계',
+    '105': 'IT/과학'
+}
+base_url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1="
+# 카테고리 선택따라 변경 필요
+category_code = '100'
+response = requests.get(f"{base_url}{category_code}", headers=headers)
 html_text = response.text
 soup = bs(html_text, 'html.parser')
 
 news = []
 len = 5
-
-news_titles = soup.select('div.sh_text>a')
+news_titles = soup.select('a.sh_text_headline')
+news_authors = soup.select('div.sh_text_press')
 for i in range(len):
     news_object = {
         'title': news_titles[i].text,
-        'href': news_titles[i].attrs['href']
+        'href': news_titles[i].attrs['href'],
+        'author': news_authors[i].text,
+        'category': category_dict[category_code],
     }
     news.append(news_object)
 
-news_authors = soup.select('div.sh_text_press')
-for i in range(len):
-    news[i]['author'] = news_authors[i].text
 
 # 각 기사 페이지 접근
 for i in range(len):
-    url = news[i]['href']
-    response = requests.get(url, headers=headers)
+    base_url = news[i]['href']
+    response = requests.get(base_url, headers=headers)
     html_text = response.text
     soup = bs(html_text, 'html.parser')
 
     news_date = soup.select_one(
-        'div.media_end_head_info_datestamp_bunch>span').attrs['data-date-time']
+        'span._ARTICLE_DATE_TIME').attrs['data-date-time']
     news[i]['date'] = news_date
 
     news_content = soup.select_one('#dic_area').text
     news[i]['content'] = news_content
     # text 긁어와도 요약에 문제없음
+
+print(news)
