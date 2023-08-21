04cd78af578e7834fa6313fae280b233716f75e3
kwonssshyeon <kwonsh4396@gmail.com>
2023-08-22 00:29:29 +0900

    chromedriver 경로설정

--- crawling/image.py
+++ crawling/image.py
@@ -34,8 +34,8 @@ def gen_image(id, search_word):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    
-    driver = webdriver.Chrome(options=chrome_options)
+
+    driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",options=chrome_options)
     driver.get("https://pollinations.ai/")
 
     WebDriverWait(driver, 300).until(EC.element_to_be_clickable(
