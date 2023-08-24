50d8c695f087029d216c284e2e05d3a876fbff44
rnjs5540 <hello.git.rnjs5540@gmail.com>
2023-08-25 05:08:27 +0900

    get_home_articles 개선, get_one_article과 get_articles 통합 및 가독성 개선

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -3,11 +3,7 @@ from bs4 import BeautifulSoup as bs
 from random import sample
 import sys
 import os
-# import keyword_naitive
-import crawling.keyword_naitive as keyword_naitive
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-# from huggingface import summarize_context
-# from summarize.huggingface import summarize_context
 
 
 headers = requests.utils.default_headers()
@@ -27,7 +23,7 @@ category_dict = {
 }
 
 
-def get_articles(category: str, userId: str):
+def get_some_articles(category: str, userId: str, num_to_get: int):
     base_url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1="
 
     category_code = category_dict[category]
@@ -37,12 +33,12 @@ def get_articles(category: str, userId: str):
 
     news_titles = soup.select('a.sh_text_headline')
     news_authors = soup.select('div.sh_text_press')
-
     news = []
-    article_num = 5
+
+    # num_to_get = 5
 
     # 카테고리 페이지에서 얻을 수 있는 기본 정보
-    for i in range(article_num):
+    for i in range(num_to_get):
         news_object = {
             'title': news_titles[i].text,
             'url': news_titles[i].attrs['href'],
@@ -50,100 +46,41 @@ def get_articles(category: str, userId: str):
             'category': category,
             'userId': userId,
         }
-        news.append(news_object)
-
-    print("페이지 기사 얻어오기")
-    # 각 기사 페이지 접근
-    for i in range(article_num):
-        base_url = news[i]['url']
-        response = requests.get(base_url, headers=headers)
-        html_text = response.text
-        soup = bs(html_text, 'html.parser')
-
-        news_date = soup.select_one('span._ARTICLE_DATE_TIME').attrs['data-date-time']
-        news[i]['date'] = news_date[0:10]
-
-        # 본문 - 기사 위쪽 굵은글씨 문단 추출 및 제거
-        naver_summary_selecors = ['#dic_area > b', '#dic_area > strong', '#dic_area > div']
-        for selector in naver_summary_selecors:
-            naver_summary = soup.select_one(selector)
-            if naver_summary:
-                naver_summary.extract()
-                break
-        strong_tags = soup.find_all('strong')
-        for strong_tag in strong_tags:
-            strong_tag.extract()
-
-        # 본문 - 이미지 설명 추출 및 제거
-        image_desc_selecors = ['#dic_area > span > em', '#dic_area > div > div > span.end_photo_org > em',
-                               '#dic_area > div:nth-child(1) > figure > figcaption']
-        for selector in image_desc_selecors:
-            image_desc = soup.select_one(selector)
-            if image_desc:
-                image_desc.extract()
-                break
-        img_desc_elements = soup.select('.img_desc')
-        for img_desc_element in img_desc_elements:
-            img_desc_element.extract()
-        img_desc_elements = soup.find_all('nbd_table')
-        for img_desc_element in img_desc_elements:
-            img_desc_element.extract()
-
-        # 본문 - 개행문자 제거 추가
-        news_content = soup.select_one('#dic_area').text.strip().replace('\n', '').replace('\\', '')
-
-        # 요약
-        news[i]['context'] = news_content[:150]
-        # 이래도 600 ~ 1700자 정도 나옴
 
         # 키워드 제목에서 추출하기
-        keyTitle = news_titles[i].text.replace('\n', ' ').replace('\\', ' ').replace('.', ' ')\
-            .replace(',',' ').replace('\'', ' ')
+        keyTitle = news_titles[i].text.replace('\n', ' ').replace('\\', ' ').replace('.', ' ').replace(',', ' ').replace(
+            '\'', ' ')
         keyTitle = list(map(str, keyTitle.split()))
+        idx = sample(range(len(keyTitle)), 5)
+        keywords = [keyTitle[i] for i in idx]
+        news_object['keywords'] = keywords
 
-        keyword_idx = sample(range(len(keyTitle)), 5)
-        keywords = []
-        for j in keyword_idx:
-            keywords.append(keyTitle[j])
-        news[i]['keywords'] = keywords
+        # 기사 본문
+        get_article_content(news_object['url'], news_object)
+
+        news.append(news_object)
+
+    return news
 
 
-    return (news)
+def get_home_articles(category: str, userId: str):
+    categories = sample(list(category_dict), 5)
+    news = []
+    for category in categories:
+        news.append(get_some_articles(category, userId, 1)[0])
+
+    return news
 
 
-def get_one_article(category: str, userId: str):
-    base_url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1="
-
-    category_code = category_dict[category]
-    response = requests.get(f"{base_url}{category_code}", headers=headers)
-    html_text = response.text
-    soup = bs(html_text, 'html.parser')
-
-    news_titles = soup.select('a.sh_text_headline')
-    news_authors = soup.select('div.sh_text_press')
-
-    news_object = {
-        'title': news_titles[0].text,
-        'url': news_titles[0].attrs['href'],
-        'author': news_authors[0].text,
-        'category': category,
-        'userId': userId,
-    }
-    news = news_object
-
-    
-
-    # 각 기사 페이지 접근
-    base_url = news['url']
-    response = requests.get(base_url, headers=headers)
+def get_article_content(url: str, news_object: dict):
+    # 기사 페이지 접근
+    response = requests.get(url, headers=headers)
     html_text = response.text
     soup = bs(html_text, 'html.parser')
 
     news_date = soup.select_one(
         'span._ARTICLE_DATE_TIME').attrs['data-date-time']
-
-    news['date'] = news_date[0:10]
-
+    news_object['date'] = news_date[0:10]
 
     # 본문 - 기사 위쪽 굵은글씨 문단 추출 및 제거
     naver_summary_selecors = ['#dic_area > b', '#dic_area > strong', '#dic_area > div']
@@ -156,9 +93,9 @@ def get_one_article(category: str, userId: str):
     for strong_tag in strong_tags:
         strong_tag.extract()
 
-
     # 본문 - 이미지 설명 추출 및 제거
-    image_desc_selecors = ['#dic_area > span > em', '#dic_area > div > div > span.end_photo_org > em', '#dic_area > div:nth-child(1) > figure > figcaption']
+    image_desc_selecors = ['#dic_area > span > em', '#dic_area > div > div > span.end_photo_org > em',
+                           '#dic_area > div:nth-child(1) > figure > figcaption']
     for selector in image_desc_selecors:
         image_desc = soup.select_one(selector)
         if image_desc:
@@ -173,54 +110,7 @@ def get_one_article(category: str, userId: str):
 
     # 본문 - 개행문자 제거 추가
     news_content = soup.select_one('#dic_area').text.strip().replace('\n', '').replace('\\', '')
-    
-    
 
     # 요약: pytorch model 서버 램 부족으로 삭제
     # news_content = summarize_context(news_content)
-    news['context'] = news_content[:150]
-
-
-
-    # 키워드 : ec2 chrome 설치문제로 실행안됨
-    # news_content_original = str(news_content)
-    # driver = keyword_naitive.init_keyword_naitive()
-    # keyword_text  = keyword_naitive.get_keyword_naitive(driver, news_content_original)
-    # keyword = keyword_text.split(',')
-    # news['keywords'] = keyword[:5]
-    # keyword_naitive.whole_sequence(news_content_original)
-
-    
-
-    # 키워드 제목에서 추출하기
-    keyTitle = news_titles[0].text.replace('\n', ' ').replace('\\', ' ').replace('.', ' ').replace(',', ' ').replace('\'', ' ')
-    keyTitle = list(map(str,keyTitle.split()))
-
-    idx = sample(range(len(keyTitle)), 5)
-    keywords = []
-    for i in idx:
-        keywords.append(keyTitle[i])
-    news['keywords'] = keywords
-
-    print(news)
-    return news
-
-
-def get_home_articles(category: str, userId: str):
-    base_url = "https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1="
-
-    categories = sample(list(category_dict), 5)
-
-    news = []
-    for category in categories:
-        category_code = category_dict[category]
-        response = requests.get(f"{base_url}{category_code}", headers=headers)
-        html_text = response.text
-        soup = bs(html_text, 'html.parser')
-
-        news_titles = soup.select('a.sh_text_headline')
-        news_authors = soup.select('div.sh_text_press')
-
-        news.append(get_one_article(category, userId))
-
-    return (news)
+    news_object['context'] = news_content[:150]
